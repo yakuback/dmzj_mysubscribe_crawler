570d347728d78b9323d2420b1c22e71e568dd918
yakuback <doushihaldmaaz@gmail.com>
2020-02-28 14:41:50 +0800

    make login_manager an independent module

--- dmzjsc_login_manager.py
+++ dmzjsc_login_manager.py
@@ -59,15 +59,3 @@ class GetMySubscribe:
 
     def close(self):
         self.session.close()
-        
-    def get_mysubscribe(self):
-        prejson = self.session.get("https://m.dmzj.com/mysubscribe", headers=self.headers)
-        prejson.raise_for_status()  # 检查状态码
-        jsonstr = prejson.text
-        if jsonstr == "":
-            print("mysubscribe json:" + jsonstr + "[failed]")
-        mysubscribe = json.loads(jsonstr)
-        with open(mysubscribe.txt, "w") as file:
-            for item in mysubscribe:
-                file.write(item)
-            print("mysubscribe write:" + file.name + "[Succeed!]")
